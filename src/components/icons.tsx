4aece5034c291fe7688c5c33bff93c050a26b7cb
Mouaad idoufkir -  (NZ4)  - معاذ ادافقير <152400161+mouuuuaad@users.noreply.github.com>
2025-11-30 23:50:59 +0100

    Fix SVG syntax in icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -11,4 +11,5 @@ export const HsebliTradeIcon = (props: React.SVGProps<SVGSVGElement>) => (
         <circle cx="12" cy="12" r="12" fill="hsl(var(--primary))"/>
         <path d="M17.5 12C17.5 15.0376 15.0376 17.5 12 17.5C8.96243 17.5 6.5 15.0376 6.5 12C6.5 8.96243 8.96243 6.5 12 6.5" stroke="hsl(var(--background))" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
+    
 );
